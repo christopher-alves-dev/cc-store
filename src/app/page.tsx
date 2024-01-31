cb78602490bc828fdc7a14cb099ce1f3ac7324d7
Christopher Alves <christopher.alves@portalsolar.com.br>
2024-01-31 20:56:59 -0300

    feat: add first discount banner to home page

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import { useSession } from "next-auth/react";
-
-export default function Home() {
-  const { data } = useSession();
-  console.log({ data: data?.user });
-  return <div>{data?.user?.name}</div>;
-}
