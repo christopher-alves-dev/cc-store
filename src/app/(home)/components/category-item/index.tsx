385a901d309f33ddb8bc6ba8afbf86094d407dac
Christopher Alves <christopher.alves@portalsolar.com.br>
2024-01-31 21:14:47 -0300

    feat: add Categories and CategoryItem component

---
+++ src/app/(home)/components/category-item/index.tsx
@@ -0,0 +1,36 @@
+import { Badge } from "@/components/ui/badge";
+import { Category } from "@prisma/client";
+import {
+  HeadphonesIcon,
+  KeyboardIcon,
+  MonitorIcon,
+  MouseIcon,
+  SpeakerIcon,
+  SquareIcon,
+} from "lucide-react";
+import React from "react";
+
+type Props = {
+  category: Category;
+};
+
+export const CategoryItem = ({ category }: Props) => {
+  const categoryIcon = {
+    mouses: <MouseIcon />,
+    keyboards: <KeyboardIcon />,
+    headphones: <HeadphonesIcon />,
+    mousepads: <SquareIcon />,
+    monitors: <MonitorIcon />,
+    speakers: <SpeakerIcon />,
+  };
+  return (
+    <Badge
+      variant="outline"
+      className="flex items-center justify-center gap-2 rounded-lg py-3"
+    >
+      {categoryIcon[category.slug]}
+
+      <span className="text-xs font-bold">{category.name}</span>
+    </Badge>
+  );
+};
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,5 @@
-"use client";
-import { useSession } from "next-auth/react";
 import Image from "next/image";
+import { Categories } from "./components/categories";
 
 export default function Home() {
   return (
@@ -13,6 +12,10 @@ export default function Home() {
         sizes="100vw"
         className="h-auto w-full"
       />
+
+      <div className="mt-8">
+        <Categories />
+      </div>
     </div>
   );
 }
