eba7ebc2d67a0209b3b74c99b4f67fd6e67b245a
Christopher Alves <christopher.alves@portalsolar.com.br>
2024-02-24 19:12:15 -0300

    feat: add category list

---
+++ src/app/catalog/components/category-item.tsx
@@ -0,0 +1,28 @@
+import { Category } from "@prisma/client";
+import Image from "next/image";
+import React from "react";
+
+type Props = {
+  category: Category;
+};
+
+export const CategoryItem = ({ category }: Props) => {
+  return (
+    <div className="flex flex-col overflow-hidden rounded-lg">
+      <div className="flex h-[150px] w-full items-center justify-center bg-gradient-to-tr from-[#5033C3] to-[#5033C333]">
+        <Image
+          src={category.imageUrl}
+          alt={category.name}
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="h-auto max-h-[70%] w-auto max-w-[80%]"
+        />
+      </div>
+
+      <div className="bg-accent py-3">
+        <p className="text-center text-sm font-semibold">{category.name}</p>
+      </div>
+    </div>
+  );
+};
--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,7 +1,12 @@
 import { Badge } from "@/components/ui/badge";
+import { prismaClient } from "@/lib/prisma";
 import { ShapesIcon } from "lucide-react";
+import React from "react";
+import { CategoryItem } from "./components/category-item";
 
-export default function page() {
+export default async function page() {
+  const categories = await prismaClient.category.findMany();
+
   return (
     <div className="flex flex-col gap-8 px-5">
       <Badge
@@ -11,6 +16,12 @@ export default function page() {
         <ShapesIcon size={16} />
         Catálogo
       </Badge>
+
+      <div className="grid grid-cols-2 gap-8">
+        {categories.map((category) => (
+          <CategoryItem key={category.id} category={category} />
+        ))}
+      </div>
     </div>
   );
 }
