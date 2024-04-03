2a6b86f54a4070ac06b16d895944bb88f325eff3
Christopher Alves <christopher.alves.dev@gmail.com>
2024-04-03 19:17:54 -0300

    feat: use initial table products

--- src/app/(admin)/dashboard/products/page.tsx
+++ src/app/(admin)/dashboard/products/page.tsx
@@ -2,9 +2,15 @@ import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { prismaClient } from "@/lib/prisma";
 import { PackageIcon, PlusIcon } from "lucide-react";
+import { ProductsTable } from "./components/products-table";
+import { computeProductTotalPrice } from "@/helpers/product";
 
 export default async function ProductsDashboardPage() {
   const products = await prismaClient.product.findMany();
+
+  const productsWithTotalPrice = products.map((product) =>
+    computeProductTotalPrice(product),
+  );
 
   return (
     <div className="flex w-full flex-col gap-10 p-10">
@@ -23,6 +29,8 @@ export default async function ProductsDashboardPage() {
           Adicionar produto
         </Button>
       </div>
+
+      <ProductsTable products={productsWithTotalPrice} />
     </div>
   );
 }
