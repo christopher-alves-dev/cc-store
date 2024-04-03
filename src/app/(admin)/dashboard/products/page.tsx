20939baf34e369e68603b3d6de7c119d12d6bb05
Christopher Alves <christopher.alves.dev@gmail.com>
2024-04-03 19:06:35 -0300

    feat: add dashboard products page

---
+++ src/app/(admin)/dashboard/products/page.tsx
@@ -0,0 +1,28 @@
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { prismaClient } from "@/lib/prisma";
+import { PackageIcon, PlusIcon } from "lucide-react";
+
+export default async function ProductsDashboardPage() {
+  const products = await prismaClient.product.findMany();
+
+  return (
+    <div className="flex w-full flex-col gap-10 p-10">
+      <Badge variant="heading">
+        <PackageIcon size={18} />
+        Produtos
+      </Badge>
+
+      <div className="flex w-full items-center justify-between">
+        <p className="text-lg font-bold">
+          Produtos encontrados: {products.length}
+        </p>
+
+        <Button className="flex gap-2">
+          <PlusIcon size={16} />
+          Adicionar produto
+        </Button>
+      </div>
+    </div>
+  );
+}
