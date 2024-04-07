78812acc8d0a409fd53deb64d04e0e4189b51392
Christopher Alves <christopher.alves.dev@gmail.com>
2024-04-07 20:20:16 -0300

    fix: add signIn with discord and github type

--- env.d.ts
+++ env.d.ts
@@ -3,6 +3,10 @@ declare namespace NodeJS {
     DATABASE_URL: string;
     GOOGLE_CLIENT_ID: string;
     GOOGLE_CLIENT_SECRET: string;
+    DISCORD_CLIENT_ID: string;
+    DISCORD_CLIENT_SECRET: string;
+    GITHUB_CLIENT_ID: string;
+    GITHUB_CLIENT_SECRET: string;
     NEXT_PUBLIC_SUPABASE_URL: string;
     NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
     NEXT_PUBLIC_STRIPE_PUBLIC_KEY: string;
@@ -10,5 +14,6 @@ declare namespace NodeJS {
     STRIPE_WEBHOOK_SECRET_KEY: string;
     NEXTAUTH_SECRET: string;
     HOST_URL: string;
+    HOST_SUCCESS_URL: string;
   }
 }
