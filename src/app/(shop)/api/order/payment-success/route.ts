cc226649bd2729fb075216a427153b36345a80ec
Christopher Alves <christopher.alves.dev@gmail.com>
2024-04-07 21:10:22 -0300

    fix: http method for payment success route

--- src/app/(shop)/api/order/payment-success/route.ts
+++ src/app/(shop)/api/order/payment-success/route.ts
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: "2023-10-16",
 });
 
-export const POST = async (request: Request) => {
+export const PUT = async (request: Request) => {
   const signature = request.headers.get("stripe-signature");
 
   if (!signature) {
