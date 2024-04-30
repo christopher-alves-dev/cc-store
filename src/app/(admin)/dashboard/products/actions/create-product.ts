3edbd312e7ece9a3976fb67b5aca1d4f1c068d64
Christopher Alves <christopher.alves.dev@gmail.com>
2024-04-30 19:24:46 -0300

    feat: ensure upload each file of formData

--- src/app/(admin)/dashboard/products/actions/create-product.ts
+++ src/app/(admin)/dashboard/products/actions/create-product.ts
@@ -4,11 +4,14 @@ import { uploadFile } from "@/actions/upload-file";
 import { productsSchema } from "../schema";
 
 export const createProduct = async (form: FormData) => {
+  const images = form.getAll("imageSelecteds") as File[];
   const rawFormData = Object.fromEntries(form.entries());
 
   const formValidated = productsSchema.safeParse(rawFormData);
 
   if (formValidated.success) {
-    await uploadFile(form);
+    images.forEach(async (image) => {
+      await uploadFile(image);
+    });
   }
 };
