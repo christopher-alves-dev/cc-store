436e7ce62091b20e01fe8a31f9d7ca9621f7416b
Christopher Alves <christopher.alves.dev@gmail.com>
2024-05-19 18:09:57 -0300

    feat: change name of file to improve normalize function

--- src/helpers/normalize.ts
+++ src/helpers/normalize.ts
@@ -1,10 +1,13 @@
 export const normalizeFileName = (file: File): File => {
   const blob = file.slice(0, file.size, file.type);
-  const normalizedFileName = file.name
-    .toLowerCase()
-    .replace(/\s/g, "-")
-    .replace(/[^\w-.]/g, "");
+  const normalizedFileName = normalize(file.name);
 
   const renamedFile = new File([blob], normalizedFileName, { type: file.type });
   return renamedFile;
 };
+
+export const normalize = (str: string): string =>
+  str
+    .toLowerCase()
+    .replace(/\s/g, "-")
+    .replace(/[^\w-.]/g, "");
