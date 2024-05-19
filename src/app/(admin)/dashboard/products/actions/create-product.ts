7d1681149433fb55fcb5ac87cb43cff83e72ebf2
Christopher Alves <christopher.alves.dev@gmail.com>
2024-05-19 18:12:30 -0300

    feat: complete create product server action

--- src/app/(admin)/dashboard/products/actions/create-product.ts
+++ src/app/(admin)/dashboard/products/actions/create-product.ts
@@ -1,19 +1,58 @@
 "use server";
 
 import { uploadFile } from "@/actions/upload-file";
+import { normalize } from "@/helpers/normalize";
+import { prismaClient } from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
 
 export const createProduct = async (form: FormData) => {
-  const images = form.getAll("imageSelecteds") as File[];
+  try {
+    const images = form.getAll("imageSelecteds") as File[];
 
-  const rawFormData = Object.fromEntries(form.entries());
+    const rawFormData = Object.fromEntries(form.entries());
+    const uploadUrlsArray: string[] = [];
 
-  const uploadUrlsArray: string[] = [];
+    const productSlug = normalize(rawFormData.name as string);
+    for (const image of images) {
+      const result = await uploadFile(image);
 
-  for (const image of images) {
-    const result = await uploadFile(image);
-
-    if (result) {
-      uploadUrlsArray.push(result);
+      if (result) {
+        uploadUrlsArray.push(result);
+      }
     }
+
+    const category = await prismaClient.category.findUnique({
+      where: { id: rawFormData.category as string },
+    });
+
+    if (!category) {
+      return {
+        error: {
+          message: "Category not found",
+        },
+      };
+    }
+
+    await prismaClient.product.create({
+      data: {
+        categoryId: category.id,
+        name: String(rawFormData.name),
+        description: String(rawFormData.description),
+        slug: productSlug,
+        basePrice: parseFloat(
+          (rawFormData.price as string).replaceAll(".", "").replace(",", "."),
+        ),
+        discountPercentage: Number(rawFormData.discountPercentage) ?? 0,
+        imageUrls: uploadUrlsArray,
+      },
+    });
+  } catch (error) {
+    return {
+      error: {
+        message: "Error creating product",
+      },
+    };
   }
+
+  revalidatePath("dashboard/products");
 };
