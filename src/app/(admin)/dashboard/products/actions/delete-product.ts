ad997bbe881316ea1f4339816e434c8e3ed24b62
Christopher Alves <christopher.alves.dev@gmail.com>
2024-05-20 19:26:51 -0300

    feat: create server action delete product

---
+++ src/app/(admin)/dashboard/products/actions/delete-product.ts
@@ -0,0 +1,21 @@
+"use server";
+
+import { prismaClient } from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
+
+export const deleteProduct = async (productId: string) => {
+  try {
+    await prismaClient.product.delete({
+      where: {
+        id: productId,
+      },
+    });
+  } catch (error) {
+    return {
+      error: {
+        message: "Error deleting product",
+      },
+    };
+  }
+  revalidatePath("dashboard/products");
+};
