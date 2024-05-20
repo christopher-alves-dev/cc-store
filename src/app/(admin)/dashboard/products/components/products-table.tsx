1dd15ea7a21f6d905c268d9e38d1f414fd187f63
Christopher Alves <christopher.alves.dev@gmail.com>
2024-05-20 19:33:01 -0300

    feat: add actions column in product table and confirmation to delete product

--- src/app/(admin)/dashboard/products/components/products-table.tsx
+++ src/app/(admin)/dashboard/products/components/products-table.tsx
@@ -1,3 +1,21 @@
+"use client";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import {
   Table,
   TableBody,
@@ -6,8 +24,12 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { toast } from "@/components/ui/use-toast";
 import { formatNumberToCurrency } from "@/helpers/format-number-to-currency";
 import { ProductWithTotalPrice } from "@/helpers/product";
+import { CircleEllipsis } from "lucide-react";
+import { useState } from "react";
+import { deleteProduct } from "../actions/delete-product";
 
 export type ProductWithTotalPriceAndCategory = ProductWithTotalPrice & {
   category: {
@@ -20,32 +42,115 @@ type Props = {
 };
 
 export const ProductsTable = ({ products }: Props) => {
+  const [openConfirmDelete, setOpenConfirmDelete] = useState(false);
+  const [productToDelete, setProductToDelete] =
+    useState<ProductWithTotalPriceAndCategory>(
+      {} as ProductWithTotalPriceAndCategory,
+    );
+
+  const handleConfirmDeleteProduct = (
+    product: ProductWithTotalPriceAndCategory,
+  ) => {
+    setProductToDelete(product);
+    setOpenConfirmDelete((prevState) => !prevState);
+  };
+
+  const handleDeleteProduct = async (
+    product: ProductWithTotalPriceAndCategory,
+  ) => {
+    const formData = new FormData();
+    formData.append("id", product.id);
+
+    const result = await deleteProduct(product.id);
+
+    if (result?.error) {
+      return toast({
+        title: "Erro!",
+        variant: "destructive",
+        description:
+          "Ops, houve um erro ao deletar o produto. Tente novamente mais tarde.",
+      });
+    }
+
+    toast({
+      title: "Sucesso!",
+      variant: "success",
+      description: "Produto deletado com sucesso!",
+    });
+  };
+
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead>Nome</TableHead>
-          <TableHead>Categoria</TableHead>
-          <TableHead>Preço total</TableHead>
-          <TableHead>Preço base</TableHead>
-          <TableHead>Vendidos</TableHead>
-        </TableRow>
-      </TableHeader>
-
-      <TableBody>
-        {products.map((product) => (
-          <TableRow key={product.id}>
-            <TableCell>{product.name}</TableCell>
-            <TableCell>{product.category.name}</TableCell>
-            <TableCell>{formatNumberToCurrency(product.totalPrice)}</TableCell>
-            <TableCell>
-              {formatNumberToCurrency(Number(product.basePrice))}
-            </TableCell>
-
-            <TableCell>0</TableCell>
+    <>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Nome</TableHead>
+            <TableHead>Categoria</TableHead>
+            <TableHead>Preço total</TableHead>
+            <TableHead>Preço base</TableHead>
+            <TableHead>Vendidos</TableHead>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHeader>
+
+        <TableBody>
+          {products.map((product) => (
+            <TableRow key={product.id}>
+              <TableCell>{product.name}</TableCell>
+              <TableCell>{product.category.name}</TableCell>
+              <TableCell>
+                {formatNumberToCurrency(product.totalPrice)}
+              </TableCell>
+              <TableCell>
+                {formatNumberToCurrency(Number(product.basePrice))}
+              </TableCell>
+
+              <TableCell>0</TableCell>
+              <TableCell>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild className="cursor-pointer">
+                    <CircleEllipsis />
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                    <DropdownMenuItem className="cursor-pointer">
+                      Editar
+                    </DropdownMenuItem>
+                    <DropdownMenuItem
+                      className="cursor-pointer"
+                      onClick={() => handleConfirmDeleteProduct(product)}
+                    >
+                      Deletar
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+
+      <AlertDialog open={openConfirmDelete} onOpenChange={setOpenConfirmDelete}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Excluir produto</AlertDialogTitle>
+            <AlertDialogDescription>
+              Você tem certeza que deseja excluir o produto{" "}
+              <span className="font-bold text-primary">
+                {productToDelete.name}?
+              </span>{" "}
+              Esta ação não poderá ser desfeita e o produto será permanentemente
+              removido.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancelar</AlertDialogCancel>
+            <AlertDialogAction
+              onClick={() => handleDeleteProduct(productToDelete)}
+            >
+              Continue
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
   );
 };
