c1daedbe852a67ac2f0f4c8edd8423ac14704810
Christopher Alves <christopher.alves.dev@gmail.com>
2024-07-13 20:00:46 -0300

    feat: turn dashboard page to private route

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,4 +1,16 @@
+import { redirect } from "next/navigation";
+
+import { createClient } from "@/lib/supabase/server";
+
 export default async function DashboardPage() {
+  const supabase = createClient();
+
+  const { data, error } = await supabase.auth.getUser();
+
+  if (error || !data?.user) {
+    redirect("/login");
+  }
+
   return (
     <div className="mt-10 flex flex-col gap-8 px-5">
       <p>Dashboard</p>
