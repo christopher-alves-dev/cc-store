7ae0c8c44323ee84afc39d55085115f7efc966f8
Christopher Alves <christopher.alves.dev@gmail.com>
2024-08-18 21:03:11 -0300

    feat: decrease metric height

--- src/app/(admin)/dashboard/components/metric-card.tsx
+++ src/app/(admin)/dashboard/components/metric-card.tsx
@@ -10,7 +10,7 @@ export const MetricCard = ({ icon, label, value = 0 }: Props) => {
   const Icon = icon;
 
   return (
-    <div className="flex h-32 flex-[1_1_200px] flex-col items-center justify-center rounded-lg border border-solid border-border px-4 lg:h-40 lg:px-0">
+    <div className="flex h-28 flex-[1_1_200px] flex-col items-center justify-center rounded-lg border border-solid border-border px-4 lg:h-40 lg:px-0">
       <div className="flex items-center gap-2">
         <div className="text-2xl">
           <Icon className="h-[1em] w-[1em] text-primary" />
